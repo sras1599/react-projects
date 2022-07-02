3c44f69a2a53b0f7db8ae02a2b5c01375a38c229
Raspreet Singh <raspreet80@gmail.com>
2022-07-03 00:09:00 +0530

    Updated deploy script for 2048 game

--- 2048/deploy.sh
+++ 2048/deploy.sh
@@ -4,14 +4,16 @@ set -e
 
 npm run build
 
-cd dist
+mkdir -p game/2048
+mv dist/* game/2048
+cd game
 
 git init
 git checkout -b main
 git add -A
-git commit -m 'deploy'
+git commit -m 'deploy 2048'
 
 git push -f git@github.com:sras1599/react-projects.git main:gh-pages
 
 cd -
-rm -rf dist/
+rm -rf game/
--- 2048/vite.config.js
+++ 2048/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-projects/"
+  base: "/react-projects/2048/"
 })
